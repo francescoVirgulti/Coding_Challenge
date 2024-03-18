5f39e58189f54ae01842732d6790d95cd5089530
francescoVirgulti <franci.virgulti@gmail.com>
2024-03-18 12:15:22 +0100

    creazione main

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int C, R, S;
+    vector<int> snakeLengths;
+
+    // Lettura dell'input
+    ifstream input("input.txt");
+    if (!input.is_open()) {
+        cout << "Impossibile aprire il file di input." << endl;
+        return 1;
+    }
+
+    input >> C >> R >> S;
+
+    snakeLengths.resize(S);
+    for (int i = 0; i < S; ++i) {
+        input >> snakeLengths[i];
+    }
+
+    vector<vector<int>> grid(R, vector<int>(C));
+
+    for (int i = 0; i < R; ++i) {
+        for (int j = 0; j < C; ++j) {
+            char cell;
+            input >> cell;
+            if (cell == '*') {
+                grid[i][j] = -1; // Segna la presenza di un wormhole con -1
+            } else {
+                grid[i][j] = cell - '0'; // Converte il carattere in intero
+            }
+        }
+    }
+
+    // Stampa della matrice inizializzata
+    cout << "Matrice inizializzata:" << endl;
+    for (int i = 0; i < R; ++i) {
+        for (int j = 0; j < C; ++j) {
+            cout << grid[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    input.close();
+
+    return 0;
+}
